57e7354f1095c1cca10f5eaf7e7e1b4973d3db83
Manuel Diez de Oñate <manudiiez@MacBook-Pro-de-Manuel.local>
2025-11-21 17:02:39 -0300

    version 1 lista para mostrar

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default async function MapPage() {
+
+  return (
+    <main className="min-h-screen pt-20">
+      {/* <MapViewServer searchParams={params} /> */}
+      <h1 className="pt-20 px-8">En desarrollo...</h1>
+    </main>
+  );
+}
+
+
+// Falta que se recargue bien al hacer zoom y cambia rde ubicacion, paginaxion, y que al seleccionar se scrollee hasta esa parte
